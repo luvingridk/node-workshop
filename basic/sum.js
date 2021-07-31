717b9e3aaaffe26aafc588e01e8392e186ef9f77
luvingridk <87885157+luvingridk@users.noreply.github.com>
2021-07-31 14:17:28 +0800

    Update sum.js

---
+++ basic/sum.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+
+function sum(n){
+    let result = 0;
+    for(let i = 0; i <= n; i++){
+        result += i;
+
+}
+return result;
+}
+console.log(sum(1));
+console.log(sum(2));
+console.log(sum(10));
