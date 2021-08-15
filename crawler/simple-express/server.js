39d3c36b2a75ce0f3b08a61c6ab76d4f3b1ce12e
luvibgridk <luvingridk@gmail.com>
2021-08-15 11:38:06 +0800

    server

--- crawler/simple-express/server.js
+++ crawler/simple-express/server.js
@@ -3,6 +3,15 @@ const express = require("express");
 // 利用 express 建立了一個 express application
 let app = express();
 
+app.use((req, res, next)=>{
+    let current = new Date();
+    console.log(`安安 at ${current.toISOString()}`);
+    next();
+  });
+  app.use((req, res, next)=>{
+    console.log("第二個");
+    next();
+  });
 // HTTP Method: get, post, put, patch, delete
 app.get("/", function (request, response, next) {
   response.send("Hello");
